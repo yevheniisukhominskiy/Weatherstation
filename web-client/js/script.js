c03e24cb86bf0571f2a14232404cc4b84d131c84
Yevhenii Sukhominskiy <yevhenii.sukhminskiy@gmail.com>
2023-08-01 13:52:53 +0300

    Created weather prediction and replacement of the corresponding icon

--- web-client/js/script.js
+++ web-client/js/script.js
@@ -1,31 +1,66 @@
-// Function to update sensor data on the web page
-function updateSensorData() 
-{
-    // Create an XMLHttpRequest object
-    var xhttp = new XMLHttpRequest();
+// Function for predicting weather based on temperature, pressure, and humidity
+function predictWeather(temperature, pressure, humidity) {
+    if (temperature < 0) {
+        return {
+            text: "Сьогодні холодно. Одягайтеся тепліше!",
+            icon: "cold"
+        };
+    } else if (temperature < 0 && humidity >= 70) {
+        return {
+            text: "Сьогодні холодно. Можливі опади в виді снігу.",
+            icon: "snow"
+        };
+    } else if (temperature >= 30) {
+        return {
+            text: "Сьогодні спекотно. Запасіться водою!",
+            icon: "hot"
+        };
+    } else if (temperature > 0 && temperature < 30) {
+        return {
+            text: "Сьогодні сонячно. День має бути гарним.",
+            icon: "sun"
+        };
+    } else if (humidity >= 60 && pressure <= 750) {
+        return {
+            text: "Сьогодні пасмурно. Можливі опади у вигляді дощу.",
+            icon: "rain"
+        };
+    } else {
+        return {
+            text: "Погода невизначена. Подивіться в вікно!",
+            icon: "error"
+        };
+    }
+}   
 
-    // Define a function to be executed when the XMLHttpRequest state changes
-    xhttp.onreadystatechange = function() 
-    {
-        // Check if the request is completed and the response is successful (status 200)
+// Function to update sensor data on the web page
+function updateSensorData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // Parse the received JSON data into a JavaScript object
             var data = JSON.parse(this.responseText);
 
-            // Update the content of HTML elements on the web page with the received sensor data
+            // Update elements with actual sensor data
             document.getElementById("temperature").innerText = data.temperature;
             document.getElementById("humidity").innerText = data.humidity;
             document.getElementById("pressure").innerText = data.pressure;
             document.getElementById("height").innerText = data.height;
+
+            // Predict the weather
+            var weatherPrediction = predictWeather(data.temperature, data.pressure, data.humidity);
+
+            // Update the element with the weather prediction text
+            document.getElementById("weatherPrediction").innerText = weatherPrediction.text;
+
+            // Set the weather icon
+            var weatherIcon = document.getElementById("weatherIcon");
+            weatherIcon.src = weatherPrediction.icon + ".svg";
         }
     };
 
-    // Configure the XMLHttpRequest to make a GET request to "/sensor-data" on the server
     xhttp.open("GET", "/sensor-data", true);
-
-    // Send the GET request
     xhttp.send();
 }
 
-// Call the updateSensorData() function every 5 seconds (5000 milliseconds)
-setInterval(updateSensorData, 5000);
+// Call the updateSensorData() function every 20 seconds
+setInterval(updateSensorData, 20000);
